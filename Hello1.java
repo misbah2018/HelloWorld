42ddff2608efe38182cf1df8e9de57b8db24022f
misbah <findmisbah2017@gmail.com>
2019-03-27 14:49:51 +0530

    commit branch1

--- Hello1.java
+++ Hello1.java
@@ -2,7 +2,11 @@
 public class  Hello1{
 	public static void main(String[] args) {
 		for (int i = 0;i< 10 ;i++ ) {
+<<<<<<< HEAD
 			System.out.println("Hello world Mirafra 1:41 :"+i);
+=======
+			System.out.println("Hello world Mirafra branch2 commit :"+i);
+>>>>>>> e304156... commit from branch2
 			
 		}
 	}
